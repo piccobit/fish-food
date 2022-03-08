7b5bd7c9950ab1aa9e71b019db2a580535650ad8
HD Stich <hd@monkeyguru.dev>
2022-03-08 14:26:03 +0100

    Switched 'gofish' to new home

--- Food/gofish.lua
+++ Food/gofish.lua
@@ -1,5 +1,5 @@
 local name = "gofish"
-local version = "0.15.1"
+local version = "0.16.0"
 
 food = {
     name = name,
@@ -11,11 +11,11 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "2be5f7be3261702e3ccaf4aa8c14b83fdde95478d49b16eb4e723c25e80bacc1",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-darwin-amd64.tar.gz",
+            sha256 = "0d040950850a98bf8b04ce4ddab1f3ed4866d0c9baf16580ff3317a5251dc5c3",
             resources = {
                 {
-                    path = "darwin-amd64/" .. name,
+                    path = name,
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -24,11 +24,11 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-darwin-arm64.tar.gz",
-            sha256 = "aaced63eae334266706976910d1eba9cb3d6ad92f952215cfdb082d6392ba1c5",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-darwin-arm64.tar.gz",
+            sha256 = "74c0b963ba926ff42bc66a41da5401449fd54b46ae683be35f561466827c1a99",
             resources = {
                 {
-                    path = "darwin-arm64/" .. name,
+                    path = name,
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -37,11 +37,24 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "23793d4445a323aee350bbde0137a172269561ff895fa4417b590b74c659ce2c",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-amd64.tar.gz",
+            sha256 = "41c6ce0ee029ac3fdec86973913e30910634e69a09c4ca858b5d3305a2633cf4",
             resources = {
                 {
-                    path = "linux-amd64/" .. name,
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "linux",
+            arch = "arm64",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-linux-arm64.tar.gz",
+            sha256 = "59cfe0e4811cd86b878abb5d6c56d5eee762baff47d6ff25789454617ddc00f9",
+            resources = {
+                {
+                    path = name,
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -50,11 +63,23 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://gofi.sh/releases/" .. name .. "-v" .. version .. "-windows-amd64.tar.gz",
-            sha256 = "387d34cf77190c6f776da1d73b9a087cb82be4e86bf875d2d207831c97ef0d5a",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-windows-amd64.zip",
+            sha256 = "4d3f52f9e490e3b207a2c91e6f12206324a72dccaec8d6ec3a0c7c71d5311506",
             resources = {
                 {
-                    path = "windows-amd64\\" .. name .. ".exe",
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
+                }
+            }
+        },
+        {
+            os = "windows",
+            arch = "arm64",
+            url = "https://github.com/piccobit/gofish/releases/download/v" .. version .. "/" .. name .. "-" .. version .. "-windows-arm64.zip",
+            sha256 = "367214d440698d92a0c4394558cddf67401357ab1d7676c3110e96406730a474",
+            resources = {
+                {
+                    path = name .. ".exe",
                     installpath = "bin\\" .. name .. ".exe"
                 }
             }
