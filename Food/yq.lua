daafc8fb3a5907a9a5a5f57a97a54ca1d2e5c888
HD Stich <hd@monkeyguru.dev>
2022-03-13 21:58:04 +0100

    Cleanup yq

--- Food/yq.lua
+++ Food/yq.lua
@@ -11,7 +11,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/mikefarah/yq/releases/download/v" .. version .. "/" .. name .. "_darwin_amd64.tar.gz",
+            url = "https://github.com/mikefarah/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_amd64.tar.gz",
             sha256 = "8ca210e1a19eb5718d7c74a75ff09391aeb0279eb24f7653737ac42fa5e8e928",
             resources = {
                 {
@@ -24,7 +24,7 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/mikefarah/yq/releases/download/v" .. version .. "/" .. name .. "_darwin_arm64.tar.gz",
+            url = "https://github.com/mikefarah/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_arm64.tar.gz",
             sha256 = "71a7484d339b43606a754eb56e840962791eb5a41abca5b68bdc55f69c7ba0d3",
             resources = {
                 {
@@ -37,7 +37,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/mikefarah/yq/releases/download/v" .. version .. "/" .. name .. "_linux_amd64.tar.gz",
+            url = "https://github.com/mikefarah/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_linux_amd64.tar.gz",
             sha256 = "149a6d22f97711c3a010eb877a3552595d8b6d6676493f55c819ba81d5466a8b",
             resources = {
                 {
@@ -50,7 +50,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/mikefarah/yq/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.zip",
+            url = "https://github.com/mikefarah/" .. name .. "/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.zip",
             sha256 = "9c4e68f83d691c8164b6143217ebf3e3c2718a31c0bb19649ac2d843c1c09956",
             resources = {
                 {
