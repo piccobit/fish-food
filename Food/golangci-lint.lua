963f4a47bb3be0e97b4ae9da3259e607374c28e2
HD Stich <hd@monkeyguru.dev>
2022-03-22 22:09:08 +0100

    golangci-lint v1.45.0

--- Food/golangci-lint.lua
+++ Food/golangci-lint.lua
@@ -1,6 +1,8 @@
 local name = "golangci-lint"
-local version = "1.44.2"
+local version = "1.45.0"
 local release = "v" .. version
+local org = "golangci"
+local repo = name
 
 food = {
     name = name,
@@ -11,8 +13,8 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/golangci/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "ec6a8f0d6b2cb65efcc7ec138d91055b2ba51433093f97f8b76ce53ab5502971",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-darwin-amd64.tar.gz",
+            sha256 = "f53826957a96a3312f5e932299024a1274b31c8b70d54510803222fb5d9f7af8",
             resources = {
                 {
                     path = name .. "-" .. version .. "-darwin-amd64/" .. name,
@@ -24,8 +26,8 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/golangci/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "461e238f83e2b3deb48665be15d835fd3eab75a9a0138074ca2ad81315e0c3aa",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-linux-amd64.tar.gz",
+            sha256 = "ca06a2b170f41a9e1e34d40ca88b15b8fed2d7e37310f0c08b7fc244c34292a9",
             resources = {
                 {
                     path = name .. "-" .. version .. "-linux-amd64/" .. name,
@@ -37,8 +39,8 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/golangci/" .. name .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-windows-amd64.zip",
-            sha256 = "8fc94fd292c4905332dcaee61dfe9104dc15713f49477c7494b63ee07a638a66",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. release .. "/" .. name .. "-" .. version .. "-windows-amd64.zip",
+            sha256 = "a8176b8ff5133de7da4393965091c429f251ecd74405289a80318f62d8284e47",
             resources = {
                 {
                     path = name .. "-" .. version .. "-windows-amd64\\" .. name .. ".exe",
