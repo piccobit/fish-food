69c13bf4a0410aac8e1eb52422c73068ed1f1c91
HD Stich <hd@monkeyguru.dev>
2022-03-23 09:34:21 +0100

    Adjusted 'kustomize' script

--- Food/kustomize.lua
+++ Food/kustomize.lua
@@ -1,5 +1,7 @@
 local name = "kustomize"
 local version = "4.5.2"
+local org = "kubernetes-sigs"
+local repo = name
 
 food = {
     name = name,
@@ -10,7 +12,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_darwin_amd64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_darwin_amd64.tar.gz",
             sha256 = "4b7dac92c8f2dd383651276c78d9e6d28031f50f3711cd987347a08edf0c8335",
             resources = {
                 {
@@ -23,7 +25,7 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_darwin_arm64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_darwin_arm64.tar.gz",
             sha256 = "4ee7ef099b8f59d65cb393d9c1b8fa49a392529dbefcd469359cc51094dad517",
             resources = {
                 {
@@ -36,7 +38,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_linux_amd64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_linux_amd64.tar.gz",
             sha256 = "c4215332da8da16ddeb88e218d8dceb76c85b366a5c58d012bc5ece904bf2fd0",
             resources = {
                 {
@@ -49,7 +51,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/kubernetes-sigs/" .. name .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_windows_amd64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/" .. name .. "%2Fv" .. version .. "/" .. name .. "_v" .. version .. "_windows_amd64.tar.gz",
             sha256 = "3c6310caa6a23d17711a312f1a33690365ba6be9a806752aac215613fdf7c605",
             resources = {
                 {
