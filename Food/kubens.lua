718129380866dfbc181628492a090442029b6362
HD Stich <hd@monkeyguru.dev>
2022-03-23 09:37:21 +0100

    Adjusted 'kubens' script

--- Food/kubens.lua
+++ Food/kubens.lua
@@ -1,5 +1,7 @@
 local name = "kubens"
 local version = "0.9.4"
+local org = "ahmetb"
+local repo = "kubectx"
 
 food = {
     name = name,
@@ -11,7 +13,7 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = "https://github.com/ahmetb/kubectx/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_darwin_x86_64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_darwin_x86_64.tar.gz",
             sha256 = "ef43ab1217e09ac1b929d4b9dd2c22cbb10540ef277a3a9b484c020820c988b1",
             resources = {
                 {
@@ -24,7 +26,7 @@ food = {
         {
             os = "darwin",
             arch = "arm64",
-            url = "https://github.com/ahmetb/kubectx/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_darwin_arm64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_darwin_arm64.tar.gz",
             sha256 = "dbae919016d4ebfa09780135cacd9d787b2d3882f13c3d5b3c3c883180496209",
             resources = {
                 {
@@ -37,7 +39,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = "https://github.com/ahmetb/kubectx/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_linux_x86_64.tar.gz",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_linux_x86_64.tar.gz",
             sha256 = "8b3672961fb15f8b87d5793af8bd3c1cca52c016596fbf57c46ab4ef39265fcd",
             resources = {
                 {
@@ -50,7 +52,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = "https://github.com/ahmetb/kubectx/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_windows_x86_64.zip",
+            url = "https://github.com/" .. org .. "/" .. repo .. "/releases/download/v" .. version .. "/" .. name .. "_v" .. version .. "_windows_x86_64.zip",
             sha256 = "eab9ace6e25303b522e7006a1c9e44747b9e9c005e15b1fcf8a9678569ca1c95",
             resources = {
                 {
