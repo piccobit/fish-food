f7f9ae60a9021f55cdb23cfb4bfd407918a28af4
HD Stich <hd@monkeyguru.dev>
2022-03-29 13:50:59 +0200

    Updated scripts

--- Food/k3d.lua
+++ Food/k3d.lua
@@ -1,7 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "k3d"
-local version = "5.4.0"
-local release = "v" .. version
+local version = "5.4.1"
 local org = "rancher"
 local repo = "k3d"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -17,8 +16,8 @@ food = {
             os = "darwin",
             arch = "amd64",
             
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-darwin-amd64",
-            sha256 = "99f8ee5f851b6f839c55b904fdc92e660d810e38399d5e01190f7d4fa0b0a382",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64",
+            sha256 = "1e008f1661a5c939cb9a6991b612ee51dd7080e6e2b1443065f3d522378e50a4",
             resources = {
                 {
                     path = name .. "-darwin-amd64",
@@ -28,10 +27,24 @@ food = {
             }
         },
         {
+            os = "darwin",
+            arch = "arm64",
+            
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-darwin-arm64",
+            sha256 = "ddb71b5963ee2d34aa4aa78a49e99a0d4bacd5db61f16e071b99d3a846afe2dc",
+            resources = {
+                {
+                    path = name .. "-darwin-arm64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
             os = "linux",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-amd64",
-            sha256 = "96c0a1f7aa65dbdc78121b1e6ad24c117fc4128f3c240003366d5e35079d9597",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-amd64",
+            sha256 = "50f64747989dc1fcde5db5cb82f8ac132a174b607ca7dfdb13da2f0e509fda11",
             resources = {
                 {
                     path = name .. "-linux-amd64",
@@ -43,8 +56,8 @@ food = {
         {
             os = "linux",
             arch = "386",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-386",
-            sha256 = "55d10b98659c5272b47b0ba7ecf35b49605de6018011e54899be477a43424a0e",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-386",
+            sha256 = "9460b9d20c765e2929b18ab0ffd37e8c25e2af05d596415060b10af1b6a60900",
             resources = {
                 {
                     path = name .. "-linux-386",
@@ -56,8 +69,8 @@ food = {
         {
             os = "linux",
             arch = "arm",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-arm",
-            sha256 = "51fe7ea801604791421a64443ec2cb04ee985d025c1cc668e7248e4de1b61f76",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-arm",
+            sha256 = "68a3db44493b121e8f48a88ce1976fda78f200a5740ca0fcd7a828cf21a5264a",
             resources = {
                 {
                     path = name .. "-linux-arm",
@@ -69,8 +82,8 @@ food = {
         {
             os = "linux",
             arch = "arm64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-arm64",
-            sha256 = "0321e06c51324b9bb6ea2ab823665289f27d4732606ed3a468b0fafc4051541b",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-arm64",
+            sha256 = "712ffb338ec1fed6f7c1c8691b79bc80967cc17fef256cd620190d0d7e502052",
             resources = {
                 {
                     path = name .. "-linux-arm64",
@@ -82,8 +95,8 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-windows-amd64.exe",
-            sha256 = "df82bc5c39f14012e45fb09d869f83a68a4dd79e354b2a322ebf7b0817577cbc",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.exe",
+            sha256 = "26721adf146621214be28f0461aeacfdd96c2a24438b35e661459d01c4defa8b",
             resources = {
                 {
                     path = name .. "-windows-amd64.exe",
