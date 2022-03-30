30dd76a9ae49c242827a7eb3a92bcc238964afca
HD Stich <hd@monkeyguru.dev>
2022-03-30 11:02:10 +0200

    Updated scripts

--- Food/stern.lua
+++ Food/stern.lua
@@ -16,7 +16,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_amd64",
-            sha256 = "ce6151449467b6015b81f7fd851f7e75ee4ee17db0259bb80c58278a7deb0954",
+            sha256 = "9525391b6a40c3815409c2ef94b4d0a60e38336119a58fb5f4133001541e1f20",
             resources = {
                 {
                     path = name .. "_darwin_amd64",
@@ -29,7 +29,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_arm64",
-            sha256 = "bb41f0cbe7caefb5223e51ac22ec13cb50b981d95e7f26faa4f6316a1774078c",
+            sha256 = "d628679468393bbcde630f6ff58a73098af3b36a810f189e7f6be287c90beb3c",
             resources = {
                 {
                     path = name .. "_darwin_arm64",
@@ -42,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_linux_amd64",
-            sha256 = "b5564d5f212aea278945025d81488df4f23f3c9c69776c3e96a28b66621ef5f2",
+            sha256 = "de9c9b20c97dad0f9ab24f51419436da698ecf80dc2c0270688e34b4b090b375",
             resources = {
                 {
                     path = name .. "_linux_amd64",
@@ -55,7 +55,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.exe",
-            sha256 = "bc1d2a7cb08262bd98431b74dddd09d78fce702d9b3c1bee93b406785e3e990a",
+            sha256 = "61d795b004ffc979c191d9f1a745d2aabfcdb1a263555f8a8b0b9eb3adbb916c",
             resources = {
                 {
                     path = name .. "_windows_amd64.exe",
