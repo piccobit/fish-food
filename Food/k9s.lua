2e0854615e799d66f8db892cd9540cd37c30e642
HD Stich <hd@monkeyguru.dev>
2022-03-30 10:40:33 +0200

    Updated scripts

--- Food/k9s.lua
+++ Food/k9s.lua
@@ -1,7 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "k9s"
 local version = "0.25.18"
-local release = "v" .. version
 local org = "derailed"
 local repo = "k9s"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,8 +15,21 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "_Darwin_x86_64.tar.gz",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Darwin_x86_64.tar.gz",
             sha256 = "d41dd24d2bb1b1ac8044b3e1a92f733d2f0422354761953930c96344da958b01",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Darwin_arm64.tar.gz",
+            sha256 = "61582a86fcd277557a4a28cf10a3d8f53e924b77b73460af2dfc729191bd1d41",
             resources = {
                 {
                     path = name,
@@ -29,7 +41,7 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "_Linux_x86_64.tar.gz",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Linux_x86_64.tar.gz",
             sha256 = "d288aacc368ab6b243fc9e7ecd17b53fa34a813509c2dc3023171085db83cf9d",
             resources = {
                 {
@@ -42,7 +54,7 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "_Windows_x86_64.tar.gz",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Windows_x86_64.tar.gz",
             sha256 = "fc695992d3e615d82f53cfaa44c97ee67bd5182e928799d763388895ee4ec19b",
             resources = {
                 {
