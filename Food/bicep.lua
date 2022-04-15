9fdba63244105e764cfa842756ade3592e838b3b
HD Stich <hd@monkeyguru.dev>
2022-04-15 21:23:56 +0200

    Updated bicep v0.5.6

--- Food/bicep.lua
+++ Food/bicep.lua
@@ -1,7 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "bicep"
-local version = "0.4.1318"
-local release = "v" .. version
+local version = "0.5.6"
 local org = "Azure"
 local repo = "bicep"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -15,9 +14,9 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-osx-x64",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-osx-x64",
             -- shasum of the release archive
-            sha256 = "0e1633ecb455edde729fbf06ef8b520e65411a764f4ab3edaab148e47b1a9e41",
+            sha256 = "41ea648c0e866193bd0f71fd2a10e41cd7215c1a2402a8f87ce4818c644b43ff",
             resources = {
                 {
                     path = name .. "-osx-x64",
@@ -29,9 +28,9 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-x64",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-x64",
             -- shasum of the release archive
-            sha256 = "474a030d62f57709b658ba5580a6718c5ae266ed530699ee6f6f0ee33b8b9190",
+            sha256 = "4cb2d07e8750ff054d2767d9e0178731bbef38cb8e29e36b9443f19e68b15fbf",
             resources = {
                 {
                     path = name .. "-linux-x64",
@@ -43,9 +42,9 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-win-x64.exe",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-win-x64.exe",
             -- shasum of the release archive
-            sha256 = "cb5bc2cf48d02b38d07d17a9f26adc796064a4b7298561187f5de189971ce2da",
+            sha256 = "24dcc880eede6170953911fc9be2f7ac3c3b6d8d21bb71726d7f8df6a0ec06ff",
             resources = {
                 {
                     path = name .. "-win-x64.exe",
