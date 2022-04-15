01e83d1a9bc374641d7a4cf2c5d7fc0f06516d6e
HD Stich <hd@monkeyguru.dev>
2022-04-15 10:02:25 +0200

    Added ARM64 for clusterctl

--- Food/clusterctl.lua
+++ Food/clusterctl.lua
@@ -17,10 +17,24 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-darwin-amd64",
             -- shasum of the release archive
-            sha256 = "3a4051190a3a8d289b09df439016ce65e6389f56c7a3a3fbf9435f8e97e1a7b1",
+            sha256 = "9a06eef95cb147f1110fc667cfea922fa7a4fcd7f66d5a41c16fd1019b97608b",
             resources = {
                 {
                     path = name .. "-darwin-amd64",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-darwin-arm64",
+            -- shasum of the release archive
+            sha256 = "8a5f88a7bf2fa804b82d464776a27cd91c55903fed7a24dd72f797a67a29cc86",
+            resources = {
+                {
+                    path = name .. "-darwin-arm64",
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -31,7 +45,7 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/" .. release .. "/" .. name .. "-linux-amd64",
             -- shasum of the release archive
-            sha256 = "1c29a9701f514a9a9c3b1e38abdc7d30ad1e7b496ec973288e86c6491a6fad06",
+            sha256 = "27c1165c45c10ff082ba2b9de635dff9a290a5c527efe715fab81617543987ba",
             resources = {
                 {
                     path = name .. "-linux-amd64",
