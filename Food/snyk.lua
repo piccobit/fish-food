b3a2368ba414bd01e8574f4c61ec80977eaeea6a
HD Stich <hd@monkeyguru.dev>
2022-04-15 16:12:19 +0200

    Updated snyk v1.906.0

--- Food/snyk.lua
+++ Food/snyk.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "snyk"
-local version = "1.889.0"
+local version = "1.906.0"
 local org = "snyk"
 local repo = "cli"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -15,7 +15,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-macos",
-            sha256 = "917edcd27d0b071aad867791af18891d3b345b34150a13de76b1aeb968bce8b5",
+            sha256 = "89f8e03e185d1e1994ae90035d3842019b19978607a15f1fe648f725601fbb7a",
             resources = {
                 {
                     path = name .. "-macos",
@@ -28,7 +28,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux",
-            sha256 = "2f821f86fa4c0541ac74d42c808a4accebd71974ca0ae677e5c692eb510e4b98",
+            sha256 = "a850e8a1b64c90628c88287c8abd139b40ad6314c77ea24d56f3ef174059dc3c",
             resources = {
                 {
                     path = name .. "-linux",
@@ -41,7 +41,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-win.exe",
-            sha256 = "ec0f623a36cf7f2022e8f190266770d94b1bf45d91f05a8715e28f11da9c0cde",
+            sha256 = "79f2367f977be8fd1e9e43226f5fe0acca4e3220072607c33c990537534b6f3c",
             resources = {
                 {
                     path = name .. "-win.exe",
