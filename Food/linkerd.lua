9069f565b7554a4ce93c6567842a606fb857de80
HD Stich <hd@monkeyguru.dev>
2022-04-15 16:05:20 +0200

    Fixed linkerd edge-22.3.5

--- Food/linkerd.lua
+++ Food/linkerd.lua
@@ -1,7 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "linkerd"
-local version = "edge-22.3.4"
-local release = "stable-" .. version
+local version = "edge-22.3.5"
 local org = "linkerd"
 local repo = "linkerd2"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,11 +15,24 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/linkerd2-cli" .. "-" .. release .. "-darwin",
-            sha256 = "ad478b4f0dcd90fec32724bd8c8a0a10e3e92443028dfa4a7c05e0f72198251e",
+            url = base_url .. "/releases/download/" .. version .. "/linkerd2-cli" .. "-" .. version .. "-darwin",
+            sha256 = "3cfc02cacf8c525a98d907d0e1d8fc31da80c73ba3373c23e69f409cf1974e77",
             resources = {
                 {
-                    path = "linkerd2-cli-" .. release .. "-darwin",
+                    path = "linkerd2-cli-" .. version .. "-darwin",
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/" .. version .. "/linkerd2-cli" .. "-" .. version .. "-darwin-arm64",
+            sha256 = "c40dbab42cfe871b9f091bf557ddba754c5f62b6105810cfe627d71297f5ca59",
+            resources = {
+                {
+                    path = "linkerd2-cli-" .. version .. "-darwin-arm64",
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -29,11 +41,11 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/linkerd2-cli" .. "-" .. release .. "-linux-amd64",
-            sha256 = "1fa3707781864b70df17f503e4efdc955ec0ed89385cc8b13c104feb1c62e0e1",
+            url = base_url .. "/releases/download/" .. version .. "/linkerd2-cli" .. "-" .. version .. "-linux-amd64",
+            sha256 = "4b46e3c5929345ebdac56058cab9c8d8fcd5f0e16fe076777391a95c3826a6f9",
             resources = {
                 {
-                    path = "linkerd2-cli-" .. release .. "-linux-amd64",
+                    path = "linkerd2-cli-" .. version .. "-linux-amd64",
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -42,11 +54,11 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/linkerd2-cli" .. "-" .. release .. "-windows.exe",
-            sha256 = "c37f8d102c9bc26bae015b767180a27e83f3c8b320066983c7b4223d1b56ff4b",
+            url = base_url .. "/releases/download/" .. version .. "/linkerd2-cli" .. "-" .. version .. "-windows.exe",
+            sha256 = "5137cd3cbdbe9a11d4658214ade967e6cae41dec6ccf4cce7fdde85a0217e33a",
             resources = {
                 {
-                    path = "linkerd2-cli-" .. release .. "-windows.exe",
+                    path = "linkerd2-cli-" .. version .. "-windows.exe",
                     installpath = "bin\\" .. name .. ".exe"
                 }
             }
