b4afde1bdaf54dfd002f16edb8948445f8868cb3
HD Stich <hd@monkeyguru.dev>
2022-04-15 16:34:52 +0200

    Updated tfsec v1.18.0

--- Food/tfsec.lua
+++ Food/tfsec.lua
@@ -1,22 +1,22 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "tfsec"
-local version = "1.15.1"
+local version = "1.18.0"
 local org = "tfsec"
-local repo = "tfsec"
+local repo = "aquasecurity"
 local base_url = "https://github.com/" .. org .. "/" .. repo
 
 food = {
     name = name,
     description = "🔒🌍 Security scanner for your Terraform code",
     license = "MIT",
-    homepage = "https://github.com/tfsec/tfsec",
+    homepage = "https://github.com/aquasecurity/tfsec",
     version = version,
     packages = {
         {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64",
-            sha256 = "a98f886805ae00b2d50cb201518005a88fb24b326bd0658f7096db1744bc2972",
+            sha256 = "de2d83456d9335f682336bc8a09f6d4aa0fc1c432ee760e9ce4a2385e4e62c8e",
             resources = {
                 {
                     path = name .. "-darwin-amd64",
@@ -29,7 +29,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-darwin-arm64",
-            sha256 = "f8fdc7bfa1943394a023d81618a5a9a016707a85e114a2fa29e3efc7bf6e4640",
+            sha256 = "dd544c4758225796a8dbbc41f53d7e8f1e77c222d437940d29f52e07eed36314",
             resources = {
                 {
                     path = name .. "-darwin-arm64",
@@ -42,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-amd64",
-            sha256 = "c49cea640d0d96d640f98a3a8811dac4d4e6eff358c598ae4d394fd2e0b5ea48",
+            sha256 = "3a56f40a68cdb58df616530419ccf5f5f151ad1ee2f69946d7d9322e3355479d",
             resources = {
                 {
                     path = name .. "-linux-amd64",
@@ -55,7 +55,7 @@ food = {
             os = "linux",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-arm64",
-            sha256 = "d7ecca32edcc5133debaf75e84496688aaf5db72b0e7c3b96a3af5e24fcbcde0",
+            sha256 = "a147aa552c1f09d4d27f7e86c2c0460da3689f7598a45415e7f00aaecb796b51",
             resources = {
                 {
                     path = name .. "-linux-arm64",
@@ -68,7 +68,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.exe",
-            sha256 = "6426ab046ba091ddb608fe63cd08daf3f5d225fda4296195fefbfe565679163d",
+            sha256 = "3d9f0bdc50e3d51b080e70e67394960f3dbc9a9ac986b1bb31880e907f754c5a",
             resources = {
                 {
                     path = name .. "-windows-amd64.exe",
