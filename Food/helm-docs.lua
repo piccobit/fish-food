6c1b3cc5391f5741e8b3cc6d4f6ad1d9fb5b39bf
HD Stich <hd@monkeyguru.dev>
2022-04-16 17:25:56 +0200

    Updated helm-docs v1.8.1

--- Food/helm-docs.lua
+++ Food/helm-docs.lua
@@ -1,10 +1,8 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "helm-docs"
-local version = "1.7.0"
-local release = "v" .. version
+local version = "1.8.1"
 local org = "norwoodj"
 local repo = "helm-docs"
-local bin = "helm-docs"
 local base_url = "https://github.com/" .. org .. "/" .. repo
 
 food = {
@@ -16,12 +14,25 @@ food = {
         {
             os = "darwin",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. bin .. "_" .. version .. "_Darwin_x86_64.tar.gz",
-            sha256 = "e34b4918ad92c6e553130029895aefc76a353f6ea7d968bbdf8037305dd22313",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_Darwin_x86_64.tar.gz",
+            sha256 = "af99e4e9c737c1cbee288f4ee3c61d80baed2f9dc4aca19e4bb7200047b52f9d",
             resources = {
                 {
-                    path = bin,
-                    installpath = "bin/" .. bin,
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_Darwin_arm64.tar.gz",
+            sha256 = "46ca7c56193f82b8ae64337583995a03cd7baefe8df6ff62ee1eb2974c814acc",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
                     executable = true
                 }
             }
@@ -29,12 +40,12 @@ food = {
         {
             os = "linux",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. bin .. "_" .. version .. "_Linux_x86_64.tar.gz",
-            sha256 = "b39ad34acd03256317692e5c671847d6f12bcd6c92adf05b3df83363d1dac20f",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_Linux_x86_64.tar.gz",
+            sha256 = "bb12ecab5595eff1f21b4311847a02c543c0c790443269327ad716f5896ceb2f",
             resources = {
                 {
-                    path = bin,
-                    installpath = "bin/" .. bin,
+                    path = name,
+                    installpath = "bin/" .. name,
                     executable = true
                 }
             }
@@ -42,12 +53,12 @@ food = {
         {
             os = "windows",
             arch = "amd64",
-            url = base_url .. "/releases/download/" .. release .. "/" .. bin .. "_" .. version .. "_Windows_x86_64.tar.gz",
-            sha256 = "22fd61aa2e31d04870ed0d11d2bdf09fb86151efc662ec0944bd571e514bc6a4",
+            url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_" .. version .. "_Windows_x86_64.tar.gz",
+            sha256 = "22fbd0427c1e408d1712be473a2c75ae1969c8d539e531d42494bf883f4b14bf",
             resources = {
                 {
-                    path = bin,
-                    installpath = "bin\\" .. bin .. ".exe"
+                    path = name .. ".exe",
+                    installpath = "bin\\" .. name .. ".exe"
                 }
             }
         }
