142d676885b5d04ec1aaa2800a897ba6f1620eb1
HD Stich <hd@monkeyguru.dev>
2022-04-16 10:51:47 +0200

    Updated ejson v1.3.3

--- Food/ejson.lua
+++ Food/ejson.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "ejson"
-local version = "1.3.2"
+local version = "1.3.3"
 local org = "Shopify"
 local repo = "ejson"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,10 +16,24 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/darwin-amd64",
             -- shasum of the release archive
-            sha256 = "676e8e27ab67c5133d75a5f0027bbc056073a17a6589cc0bd15f54c9e01a6003",
+            sha256 = "e6e4323384aebcdfd29c06cc350e43f0c28d4515da5e01547b05ff18ab89b51e",
             resources = {
                 {
                     path = "darwin-amd64", 
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/v" .. version .. "/darwin-arm64",
+            -- shasum of the release archive
+            sha256 = "d7b123ce25469c5326638f06f9536f9c61a86866558a1ef38fef1ba93f7dbc4c",
+            resources = {
+                {
+                    path = "darwin-arm64", 
                     installpath = "bin/" .. name,
                     executable = true
                 }
@@ -30,7 +44,7 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/linux-amd64",
             -- shasum of the release archive
-            sha256 = "777b5bec45ba569c16b5a90c5e05c924794b74b8603b2148556f361745cfa58b",
+            sha256 = "477de88e0496163b931f6d2294f1eb75f022bcd341218f80edd92b7346fc37af",
             resources = {
                 {
                     path = "linux-amd64",
@@ -43,7 +57,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/windows-amd64.exe",
-            sha256 = "16cbf0d49e0b7eb3833818abe60489acf2fcd502c7e76c0d2b5d75486bf901ab",
+            sha256 = "46bfa49a5ae924629d3d124bc2ef54cb529b05277d3be360862b76009166adee",
             resources = {
                 {
                     path = "windows-amd64.exe",
