ea856b4a9b6ec128dceee5d2d2c88b822729f21a
HD Stich <hd@monkeyguru.dev>
2022-04-18 09:12:26 +0200

    Updated glooctl v1.12.0-beta5

--- Food/glooctl.lua
+++ Food/glooctl.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "glooctl"
-local version = "1.11.4"
+local version = "1.12.0-beta5"
 local org = "solo-io"
 local repo = "gloo"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,7 +16,7 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-darwin-amd64",
             -- shasum of the release archive
-            sha256 = "743cad43326e141cc23c182e0cb621dd448431bf80f90b1a9c07aa3a34f25783",
+            sha256 = "17be4082aebe5a7c4030b51ed14279574d224e22d691b01a75bbea0e01d1137c",
             resources = {
                 {
                     path = name .. "-darwin-amd64",
@@ -30,7 +30,7 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-linux-amd64",
             -- shasum of the release archive
-            sha256 = "afb0777ef906f6869ea8e2c958f9f40d601145f0274ec1ae9597a1c2eeb0e0e0",
+            sha256 = "999d6832363c1a812fbd52104a802962686665af5840d30d52104e40a9f58050",
             resources = {
                 {
                     path = name .. "-linux-amd64",
@@ -44,7 +44,7 @@ food = {
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "-windows-amd64.exe",
             -- shasum of the release archive
-            sha256 = "b56355d57027db7fdbe7eb634e9a8d22cccb24402f9f82a711b4eba60c9eaaf8",
+            sha256 = "f5d9d4734ec0e426b264156bacb01059b26d1f29aeb1b27e5bbcaeb30909c810",
             resources = {
                 {
                     path = name .. "-windows-amd64" .. ".exe",
