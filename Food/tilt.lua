37a23323c481a02f62c494faa1ce77767c404d59
HD Stich <hd@monkeyguru.dev>
2022-04-21 20:13:18 +0200

    Updated tilt v0.27.2

--- Food/tilt.lua
+++ Food/tilt.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "tilt"
-local version = "0.27.1"
+local version = "0.27.2"
 local org = "tilt-dev"
 local repo = "tilt"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,7 +16,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "." .. version .. ".mac.x86_64.tar.gz",
-            sha256 = "aab67b451e5af0919a1e46ecafe323d7f67cb740af1013e81182750dccf43134",
+            sha256 = "331dda34c92e8c1b28edec3eee1630d5468d3fc86ce243562e27712e5b3a66c6",
             resources = {
                 {
                     path = name,
@@ -29,7 +29,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "." .. version .. ".mac.arm64.tar.gz",
-            sha256 = "1258e3bf75b9d0eaa53497b6fd265d4c8505b0f20ebb2e33e4d30fdb2d587833",
+            sha256 = "60e370c343047bf231a15a97e27d9cb0258b2134e458d947c66ccdde9b9d64e3",
             resources = {
                 {
                     path = name,
@@ -42,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "." .. version .. ".linux.x86_64.tar.gz",
-            sha256 = "c109f96437a19671d79a9a7c6a0f2ce3d2792fb3d638750052b6d2ecdb80398f",
+            sha256 = "3e4eba61f03fdfeb18a88c053b18a6355bcbe3cfe0d1ebe06d448846b2654b90",
             resources = {
                 {
                     path = name,
@@ -55,7 +55,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "." .. version .. ".windows.x86_64.zip",
-            sha256 = "f5b88aca2b67f7f1aa0a3a1849ed0be87e07838ed4403d261fd76b7f1ee6b6ba",
+            sha256 = "6de0545cdca3356e87249506dbc247cf1d1bfdd9a18220dd8dc0f6a20b033b8a",
             resources = {
                 {
                     path = name .. ".exe",
