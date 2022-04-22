018dc5ce5d4fe391afb141f4dd91cb5356d61c89
HD Stich <hd@monkeyguru.dev>
2022-04-22 19:48:59 +0200

    Updated istioctl v1.13.3

--- Food/istioctl.lua
+++ Food/istioctl.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "istioctl"
-local version = "1.13.2"
+local version = "1.13.3"
 local org = "istio"
 local repo = "istio"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,7 +16,20 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/" .. version .. "/istio" .. "-" .. version .. "-osx.tar.gz",
-            sha256 = "5569958ac9d245cc3225d8062e0724b05622c70bb3e567df640ef6e052161ac1",
+            sha256 = "b8a0ea79506d16fe1710ef18535c5415e71545469dde6065f7f591162e4f011c",
+            resources = {
+                {
+                    path = "istio-" .. version .. "/bin/" .. name ,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = base_url .. "/releases/download/" .. version .. "/istio" .. "-" .. version .. "-osx-arm64.tar.gz",
+            sha256 = "414b48ea57bde389e4cade8ff7ec0013ecc7ad682b8a1962edd9a7c2715a3931",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name ,
@@ -29,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/" .. version .. "/istio" .. "-" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "a1c30fe5c612fce80a19fe3d221ddac43f8fc6395e0f18d12bd34320f059dea7",
+            sha256 = "5dfed06990a2b2337f02617c4843e8e444df1c8456bad93f570892ec6e6e4cd9",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name ,
@@ -42,7 +55,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/" .. version .. "/istio" .. "-" .. version .. "-win.zip",
-            sha256 = "f79e841f5dde317828106a4fb6b451aa562fce4cad52fa508fff0fcfea217d81",
+            sha256 = "3324638baf77132fb96b23e69d8290e007d3b06c87362f42c223dc8c14b79e32",
             resources = {
                 {
                     path = "istio-" .. version .. "/bin/" .. name .. ".exe",
