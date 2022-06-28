5215076569721c83f074c3307f88d92c6b2fd1ef
HD Stich <hd@monkeyguru.dev>
2022-06-28 07:26:05 +0200

    Go v1.18.3

--- Food/go.lua
+++ Food/go.lua
@@ -1,6 +1,6 @@
 -- type = google -- DO NOT REMOVE THIS LINE!
 local name = "go"
-local release = "1.18.2"
+local release = "1.18.3"
 local version = release
 
 food = {
@@ -14,7 +14,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://go.dev/dl/go" .. release .. ".darwin-amd64.tar.gz",
-            sha256 = "1f5f539ce0baa8b65f196ee219abf73a7d9cf558ba9128cc0fe4833da18b04f2",
+            sha256 = "d9dcf8fc35da54c6f259be41954783a9f4984945a855d03a003a7fd6ea4c5ca1",
             resources = {
                 {
                     path = "go/bin/go",
@@ -32,7 +32,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = "https://go.dev/dl/go" .. release .. ".darwin-arm64.tar.gz",
-            sha256 = "6c7df9a2405f09aa9bab55c93c9c4ce41d3e58127d626bc1825ba5d0a0045d5c",
+            sha256 = "40ecd383c941cc9f0682e6a6f2a333539d58c7dea15c842434d03afafe2f7242",
             resources = {
                 {
                     path = "go/bin/go",
@@ -50,7 +50,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://go.dev/dl/go" .. release .. ".linux-amd64.tar.gz",
-            sha256 = "e54bec97a1a5d230fc2f9ad0880fcbabb5888f30ed9666eca4a91c5a32e86cbc",
+            sha256 = "956f8507b302ab0bb747613695cdae10af99bbd39a90cae522b7c0302cc27245",
             resources = {
                 {
                     path = "go/bin/go",
@@ -68,7 +68,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://go.dev/dl/go" .. release .. ".windows-amd64.zip",
-            sha256 = "41fc44109c39a98e0c3672989ac5ad205cbb5768067e099dc4fb2b75cba922cf",
+            sha256 = "9c46023f3ad0300fcfd1e62f2b6c2dfd9667b1f2f5c7a720b14b792af831f071",
             resources = {
                 {
                     path = "go\\bin\\go.exe",
