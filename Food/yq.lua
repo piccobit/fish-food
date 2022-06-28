9cf6a2929f6cac45db007572ae28c77cc80b05ff
HD Stich <hd@monkeyguru.dev>
2022-06-28 09:19:10 +0200

    yq v4.25.3

--- Food/yq.lua
+++ Food/yq.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "yq"
-local version = "4.24.5"
+local version = "4.25.3"
 local org = "mikefarah"
 local repo = "yq"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,7 +16,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_amd64.tar.gz",
-            sha256 = "e71789c47c672ba204ebae793fec47e3de7c2e84651103fa92703234cfbae5ec",
+            sha256 = "5a8f5199c658b98ad975fe198348504130fc2fa890ad6e88bd6147606b3e395f",
             resources = {
                 {
                     path = name .. "_darwin_amd64",
@@ -29,7 +29,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_darwin_arm64.tar.gz",
-            sha256 = "8c214ac21cf4691324f53f16559009febd8effb5a8de208a5342d6431d3d98fe",
+            sha256 = "2338def032571320b81219b0742ba80e31f37afcf2e00741ba9e50a739c278e4",
             resources = {
                 {
                     path = name .. "_darwin_arm64",
@@ -42,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_linux_amd64.tar.gz",
-            sha256 = "c00205cd323aff661a3b119a4105ca636cd79ce2486d3a3fc531755bb022df66",
+            sha256 = "3e32ceef03d0bd1ac2be13e4e01e06a4643052505ec1323c5396f21c66532411",
             resources = {
                 {
                     path = name .. "_linux_amd64",
@@ -55,7 +55,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_windows_amd64.zip",
-            sha256 = "d997eac236e712d551f6ee1c3cea754410e71055869f8926225ef44ff7f911e4",
+            sha256 = "3f5a45b715f2683f9171c666afd108f1fb49488fb887c319404a55dfe6a8fee0",
             resources = {
                 {
                     path = name .. "_windows_amd64.exe",
