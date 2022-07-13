1247ae3f75ca69396f6d77321bd0ba0877032f97
HD Stich <fish@gofish.dev>
2022-07-13 15:22:35 +0200

    GoReleaser v1.10.2

--- Food/goreleaser.lua
+++ Food/goreleaser.lua
@@ -1,6 +1,6 @@
 -- type = github -- DO NOT REMOVE THIS LINE!
 local name = "goreleaser"
-local version = "1.9.2"
+local version = "1.10.2"
 local org = "goreleaser"
 local repo = "goreleaser"
 local base_url = "https://github.com/" .. org .. "/" .. repo
@@ -16,7 +16,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Darwin_x86_64.tar.gz",
-            sha256 = "ea95b4dde744fe25872a6554d591a5cc048572abace6b1c8dd9e4567ff10aef4",
+            sha256 = "9169f36e10f24c6c64f2776d5580a2a3cee707672bb30811e19cbc1c5f8e7923",
             resources = {
                 {
                     path = name,
@@ -29,7 +29,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Darwin_arm64.tar.gz",
-            sha256 = "0aa50be54f0e487e55144f8473846d78a227ea3bb27fe9590d4cdeec60cb46c2",
+            sha256 = "1414560c2ddad2aac7d999a55252791d6941352498eff37cb7d4aa0dc6c26f76",
             resources = {
                 {
                     path = name,
@@ -42,7 +42,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Linux_x86_64.tar.gz",
-            sha256 = "6089679e325a4cfc1d1374710dca99cd6f1f5ae4f351ceffa460d599432963d7",
+            sha256 = "df5607bdd648bf44eeb1af9bb03f65fd04427b55164d2eb07d6a58baa9c7ad66",
             resources = {
                 {
                     path = name,
@@ -55,7 +55,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = base_url .. "/releases/download/v" .. version .. "/" .. name .. "_Windows_x86_64.zip",
-            sha256 = "46b99a3aed1484358aa415910204066863510c646444bcbc92b0b186ece6e611",
+            sha256 = "e038d88d7906b81120a16cb6520ab5441fc725dca4d3434ea021cd7457fd4f5f",
             resources = {
                 {
                     path = name .. ".exe",
