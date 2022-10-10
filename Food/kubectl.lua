cd089b39dd5a4e7cdd43769bd7c688e0780b0972
HD Stich <fish@gofish.dev>
2022-10-10 08:11:47 +0200

    kubectl v1.25.2

--- Food/kubectl.lua
+++ Food/kubectl.lua
@@ -1,6 +1,6 @@
 -- type = k8s -- DO NOT REMOVE THIS LINE!
 local name = "kubectl"
-local version = "1.23.4"
+local version = "1.25.2"
 
 food = {
     name = name,
@@ -13,7 +13,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-darwin-amd64.tar.gz",
-            sha256 = "9b2cc45a1fdc5d634e6bfffe9d80ecd5d0badf70cec07cb0860fe4f7619c6e3a",
+            sha256 = "eb5655cdf28ebaee84a88b9ed010befe2b92c57bf702080b2d26c763a678e532",
             resources = {
                 {
                     path = "kubernetes/client/bin/" .. name,
@@ -26,7 +26,7 @@ food = {
             os = "darwin",
             arch = "arm64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-darwin-arm64.tar.gz",
-            sha256 = "192bc95cece2698fb011d55e834b1c1299f5c1ca15c8d1ba9285121a679b19d6",
+            sha256 = "77ecddf34548c6451b944848c3d0c8c859ef1810f7394a736efbd618d2d4ccf1",
             resources = {
                 {
                     path = "kubernetes/client/bin/" .. name,
@@ -39,7 +39,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-linux-amd64.tar.gz",
-            sha256 = "23d635e507e66484b442fe242cc281a251b7f15f4a5520bfbd357a8d749ad22a",
+            sha256 = "2996dcf77e7841acd02d031f3f0b41324545619a5c6bda1fac82bb6bb8963f81",
             resources = {
                 {
                     path = "kubernetes/client/bin/" .. name,
@@ -52,7 +52,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://dl.k8s.io/v" .. version .. "/kubernetes-client-windows-amd64.tar.gz",
-            sha256 = "a2e8c40f4d3685b306dfc94f2c72898a629d3434083fdd21976a936d93dd0e38",
+            sha256 = "44030a7f875d1cb7a49a274ba1af1c0db6183af08eec1b5d92b4b0c03fb8ce09",
             resources = {
                 {
                     path = "kubernetes\\client\\bin\\" .. name .. ".exe",
